4f00090a140efb8204a9e1a15d0e939d93fa7219
Oleksandr Shchur <osur12709@gmail.com>
2026-05-28 12:04:05 +0300

    update schema

--- PriceSaver.Server/docs/sql/schema.sql
+++ PriceSaver.Server/docs/sql/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE [dbo].[Subscriptions] (
     [ProductUrl] NVARCHAR(2000) NOT NULL,
     [StoreType] INT NOT NULL,
     [ProductName] NVARCHAR(500) NULL,
-    [CurrentPrice] DECIMAL(18,4) NOT NULL,
+    [CurrentPrice] DECIMAL(18,2) NOT NULL,
     [LastCheckedDate] DATETIME2 NULL,
     [IsActive] BIT NOT NULL DEFAULT 1,
     [NotifyOnIncrease] BIT NOT NULL DEFAULT 0,
@@ -22,6 +22,6 @@ CREATE TABLE [dbo].[Subscriptions] (
 CREATE TABLE [dbo].[PriceHistories] (
     [Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
     [SubscriptionId] UNIQUEIDENTIFIER NOT NULL,
-    [Price] DECIMAL(18,4) NOT NULL,
+    [Price] DECIMAL(18,2) NOT NULL,
     [CheckedAt] DATETIME2 NOT NULL DEFAULT GETUTCDATE()
 );
