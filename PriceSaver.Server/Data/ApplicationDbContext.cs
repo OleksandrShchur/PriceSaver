272322ffea14d126fe687602709877b3fc06ca03
Oleksandr Shchur <71557324+OleksandrShchur@users.noreply.github.com>
2026-06-14 10:44:04 +0300

    PS-24 - remove maudau table and job (#22)
    * Revert "PS-23 - scrape maudau market (#21)"
    
    This reverts commit 2e40a3396513ee1f9652831caacd55a7b42db5b0.
    
    * remove sensitive information from appsettings used for local testing

--- PriceSaver.Server/Data/ApplicationDbContext.cs
+++ PriceSaver.Server/Data/ApplicationDbContext.cs
@@ -10,7 +10,6 @@ namespace PriceSaver.Server.Data
         public DbSet<User> Users => Set<User>();
         public DbSet<Subscription> Subscriptions => Set<Subscription>();
         public DbSet<PriceHistory> PriceHistories => Set<PriceHistory>();
-        public DbSet<MaudauMarket> MaudauProducts => Set<MaudauMarket>();
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -32,20 +31,6 @@ namespace PriceSaver.Server.Data
             modelBuilder.Entity<PriceHistory>(b =>
             {
                 b.HasKey(p => p.Id);
-            });
-
-            modelBuilder.Entity<MaudauMarket>(b =>
-            {
-                b.ToTable("MaudauMarket");
-                b.HasKey(m => m.Id);
-
-                b.Property(m => m.Slug).IsRequired().HasMaxLength(512);
-                b.Property(m => m.Title).IsRequired().HasMaxLength(1000);
-                b.Property(m => m.Price).HasColumnType("decimal(18,2)");
-                b.Property(m => m.OldPrice).HasColumnType("decimal(18,2)");
-
-                b.HasIndex(m => m.ProductId).IsUnique().HasDatabaseName("IX_MaudauMarket_ProductId");
-                b.HasIndex(m => m.Slug).HasDatabaseName("IX_MaudauMarket_Slug");
             });
         }
     }
